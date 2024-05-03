bd6f190baf4bb1130e37b713322640c99419e55c
Natzgun <itzlilouxx@gmail.com>
2024-05-03 08:29:18 -0500

    Add: Se han definido los atributos del disco que se cargara por defecto

--- src/storage/Disk.cpp
+++ src/storage/Disk.cpp
@@ -1,1 +1,20 @@
 #include "storage/Disk.h"
+#include <iostream>
+using namespace std;
+
+Disk::Disk() {
+  this->numPlatters = 8;
+  this->tracksPerSurface = 16384;
+  this->sectorsPerTrack = 128;
+  this->bytesPerSector = 4096;
+}
+
+Disk::Disk(int plates, int tracks, int sector, int bytes) : numPlatters(plates), tracksPerSurface(tracks), sectorsPerTrack(sector), bytesPerSector(bytes){
+}
+
+void Disk::capacityDisk() {
+  cout << "Disk capacity: " << numPlatters * 2 * tracksPerSurface * sectorsPerTrack * bytesPerSector << " bytes" << endl;
+}
+
+void Disk::generateDiskStructure() {
+}
